ac378286f05714cf600b6a12d14937c296c4d80d
Preet Mistry <pmistry9597@gmail.com>
2022-03-06 19:35:11 -0500

    SIGMOID INSTEAD OF SOFTMAX ON BASIC MODEL FACEPALM

--- basic_model.py
+++ basic_model.py
@@ -10,14 +10,14 @@ class BasicModel(torch.nn.Module):
         self.layer1 = torch.nn.Linear(in_features=256, out_features=256)
         self.layer2 = torch.nn.Linear(in_features=256, out_features=24*64*64)
         self.relu = torch.nn.ReLU()
-        self.softmax = torch.nn.Softmax(dim=-1)
+        self.sigmoid = torch.nn.Sigmoid()
     
     def forward(self, features):
         x = features.view([-1, 12*128*128]) / 1024.0
 
         x = self.relu(self.layer0(x))
         x = self.relu(self.layer1(x))
-        x = self.softmax(self.layer2(x))
+        x = self.sigmoid(self.layer2(x))
 
         return x.view([-1, 24, 64, 64]) * 1024.0
 
