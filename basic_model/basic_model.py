adb18ecde4e6cf695482b0c824e5edaa6ab8a4a8
Preet Mistry <pmistry9597@gmail.com>
2022-03-08 04:11:18 -0500

    Slight model upgrade for basic

--- basic_model/basic_model.py
+++ basic_model/basic_model.py
@@ -6,9 +6,9 @@ class BasicModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer0 = torch.nn.Linear(in_features=12*128*128, out_features=256)
-        self.layer1 = torch.nn.Linear(in_features=256, out_features=256)
-        self.layer2 = torch.nn.Linear(in_features=256, out_features=24*64*64)
+        self.layer0 = torch.nn.Linear(in_features=12*128*128, out_features=512)
+        self.layer1 = torch.nn.Linear(in_features=512, out_features=512)
+        self.layer2 = torch.nn.Linear(in_features=512, out_features=24*64*64)
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
     
--- submission/model.py
+++ submission/model.py
@@ -6,9 +6,9 @@ class BasicModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.layer0 = torch.nn.Linear(in_features=12*128*128, out_features=256)
-        self.layer1 = torch.nn.Linear(in_features=256, out_features=256)
-        self.layer2 = torch.nn.Linear(in_features=256, out_features=24*64*64)
+        self.layer0 = torch.nn.Linear(in_features=12*128*128, out_features=512)
+        self.layer1 = torch.nn.Linear(in_features=512, out_features=512)
+        self.layer2 = torch.nn.Linear(in_features=512, out_features=24*64*64)
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
     
@@ -20,5 +20,3 @@ class BasicModel(torch.nn.Module):
         x = self.sigmoid(self.layer2(x))
 
         return x.view([-1, 24, 64, 64]) * 1024.0
-
-
