2bd0cd4ce26ddcd3ebddd2e5c1ab35964c8d8a1a
Preet Mistry <pmistry9597@gmail.com>
2022-03-15 17:10:37 +0000

    Transformer unet w/o skip and deep unet

--- unet/model.py
+++ unet/model.py
@@ -110,6 +110,105 @@ class UNet(torch.nn.Module):
         x = self.sigmoid(x) * self.range
 
         return x
+    
+class UNetDeep(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=2)
+        
+        # down sample convolutions
+        self.inConv = DoubleConv(12, 128)
+        self.downConv0 = DoubleConv(128, 256)
+        self.downConv1 = DoubleConv(256, 512)
+        self.downConv2 = DoubleConv(512, 1024)
+        
+        self.encoderFinalConv = torch.nn.Sequential(
+            torch.nn.Conv2d(1024, 2048, kernel_size=3),
+            torch.nn.BatchNorm2d(2048),
+            torch.nn.GELU(),
+            torch.nn.Conv2d(2048, 4096, kernel_size=3),
+            torch.nn.BatchNorm2d(4096),
+            torch.nn.GELU(),
+            torch.nn.Conv2d(4096, 8192, kernel_size=3),
+            torch.nn.BatchNorm2d(8192),
+            torch.nn.GELU(),
+        )
+        
+        self.decoderInitialConv = torch.nn.Sequential(
+            torch.nn.ConvTranspose2d(8192, 4096, kernel_size=3),
+            torch.nn.BatchNorm2d(4096),
+            torch.nn.GELU(),
+            torch.nn.ConvTranspose2d(4096, 2048, kernel_size=3),
+            torch.nn.BatchNorm2d(2048),
+            torch.nn.GELU(),
+            torch.nn.ConvTranspose2d(2048, 1024, kernel_size=3),
+            torch.nn.BatchNorm2d(1024),
+            torch.nn.GELU(),
+        )
+        
+        # upsample portion of network
+        self.upSample0 = torch.nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.upConv0 = DoubleConv(1024, 512, padding=2)
+
+        self.upSample1 = torch.nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upConv1 = DoubleConv(512, 256)
+
+        self.upSample2 = torch.nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upConv2 = DoubleConv(256, 128, kernel_size=5)
+
+        self.finalLayer = torch.nn.Sequential(
+            torch.nn.Conv2d(128, 24, kernel_size=1)
+        )
+
+        self.crop = Crop()
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.range = 1024.0
+
+    def forward(self, in_val):
+        x = in_val / self.range
+
+        x0 = self.inConv(x)
+        x1 = self.downConv0(self.maxpool(x0))
+        x2 = self.downConv1(self.maxpool(x1))
+        x3 = self.downConv2(self.maxpool(x2))
+        
+#         print(x0.shape)
+#         print(x1.shape)
+#         print(x2.shape)
+#         print(x3.shape)
+        
+        x3 = self.encoderFinalConv(x3)
+        x3 = self.decoderInitialConv(x3)
+        
+#         print('after downsample')
+        
+        x = self.upSample0(x3)
+        # print(x.shape)
+        x2 = self.crop(x2, x)
+        x = self.upConv0( torch.cat([x, x2], dim=1) )
+#         print(x.shape)
+        
+        x = self.upSample1(x)
+        # print(x.shape)
+        x1 = self.crop(x1, x)
+        x = self.upConv1( torch.cat([x, x1], dim=1) )
+#         print(x.shape)
+        
+        x = self.upSample2(x)
+        # print(x.shape)
+        x0 = self.crop(x0, x)
+        x = self.upConv2( torch.cat([x, x0], dim=1) )
+#         print(x.shape)
+
+        x = self.finalLayer(x)
+        # print(x.shape)
+
+        x = self.sigmoid(x) * self.range
+
+        return x
 
 
 
@@ -393,7 +492,7 @@ class TransformerUNet(torch.nn.Module):
         self.downConv1 = DoubleConv(256, 512)
         self.downConv2 = DoubleConv(512, 1024)
 
-        self.latentTransform = Transformer(8*8, 8*8*8, 12, 24, 1024, 1024)
+        self.latentTransform = Transformer(8*8, 8*8*4, 8, 8, 1024, 1024)
         self.latentNorm = torch.nn.LayerNorm(8*8)
         
         self.dropout = torch.nn.Dropout(0.1)
@@ -439,6 +538,104 @@ class TransformerUNet(torch.nn.Module):
         # transformer on latent space + res connection
         x = self.latentTransform(x3)
         x = self.latentNorm(self.dropout(x) + x3)
+
+        x = x.view([-1, 1024, 8, 8])
+        
+        x = self.upSample0(x)
+        # print(x.shape)
+        x2 = self.crop(x2, x)
+        x = self.upConv0( torch.cat([x, x2], dim=1) )
+#         print(x.shape)
+        
+        x = self.upSample1(x)
+        # print(x.shape)
+        x1 = self.crop(x1, x)
+        x2 = self.crop(x2, x)
+        x = self.upConv1( torch.cat([x, x1], dim=1) )
+#         print(x.shape)
+        
+        x = self.upSample2(x)
+        # print(x.shape)
+        x0 = self.crop(x0, x)
+        x = self.upConv2( torch.cat([x, x0], dim=1) )
+#         print(x.shape)
+
+        # print(x0.shape)
+        # print(x1.shape)
+        # print(x2.shape)
+        # print(x3.shape)
+
+        x = self.finalLayer(x)
+        # print(x.shape)
+
+        x = self.sigmoid(x) * self.range
+
+        return x
+
+class TransformerUNetNoSkip(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=2)
+        
+        # down sample convolutions
+        self.inConv = DoubleConv(12, 128)
+        self.downConv0 = DoubleConv(128, 256)
+        self.downConv1 = DoubleConv(256, 512)
+        self.downConv2 = DoubleConv(512, 1024)
+        
+        embedding = torch.empty([64, 64])
+        torch.nn.init.xavier_normal_(embedding)
+#         print(embedding)
+        self.embedding = torch.nn.Parameter(embedding)
+        
+        self.latentTransform = Transformer(8*8, 8*8*4, 8, 8, 1024, 1024)
+#         self.latentNorm = torch.nn.LayerNorm(8*8)
+        
+        self.dropout = torch.nn.Dropout(0.1)
+        
+        # upsample portion of network
+        self.upSample0 = torch.nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.upConv0 = DoubleConv(1024, 512, padding=2)
+
+        self.upSample1 = torch.nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upConv1 = DoubleConv(512, 256)
+
+        self.upSample2 = torch.nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upConv2 = DoubleConv(256, 128, kernel_size=5)
+
+        self.finalLayer = torch.nn.Sequential(
+            torch.nn.Conv2d(128, 24, kernel_size=1)
+        )
+
+        self.crop = Crop()
+
+        self.sigmoid = torch.nn.Sigmoid()
+
+        self.range = 1024.0
+
+    def forward(self, in_val):
+        x = in_val / self.range
+
+        x0 = self.inConv(x)
+        x1 = self.downConv0(self.maxpool(x0))
+        x2 = self.downConv1(self.maxpool(x1))
+        x3 = self.downConv2(self.maxpool(x2))
+        
+        # print(x0.shape)
+        # print(x1.shape)
+        # print(x2.shape)
+        # print(x3.shape)
+        
+#         print('after downsample')
+
+        # kv tensor for all attention mechanisms
+        x3 = x3.view([-1, 1024, 64])
+
+        # transformer on latent space
+        x = torch.matmul(x3, self.embedding)
+        x = self.latentTransform(x3)
+#         x = self.latentNorm(self.dropout(x) + x3)
 
         x = x.view([-1, 1024, 8, 8])
         
